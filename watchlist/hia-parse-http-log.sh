7d66a89a88058bd88a69a61daffc1e55f16e866f
ircerr <ircerr@HypeIRC>
2024-08-04 10:31:45 -0400

    updated hia-parse-http-log.sh

--- watchlist/hia-parse-http-log.sh
+++ watchlist/hia-parse-http-log.sh
@@ -2,7 +2,9 @@
 
 touch hia-parse-http-log.db
 
-grep peers.txt /var/log/lighttpd/cjdns.ca.access.log | \
+grep peers.txt \
+/var/log/lighttpd/cjdns.ca.access.log \
+/var/log/lighttpd/hia.cjdns.ca.access.log | \
 cut -d\" -f4 | grep -v '^-$' | sort | uniq | \
 while read U
 do
