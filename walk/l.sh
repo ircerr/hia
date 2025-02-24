a3081d77ea78fce3a2dc0ebdefa12f393d7dac18
ircerr <ircerr@HypeIRC>
2025-02-24 11:01:34 -0500

    added l.sh

---
+++ walk/l.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+while :;
+do
+  ./walk.sh
+  if [ -x v.sh ]
+  then
+    ./v.sh
+  fi
+  if [ -x top.sh ]
+  then
+    ./top.sh
+  fi
+  if [ -x shame.sh ]
+  then
+    ./shame.sh
+  fi
+  if [ -x /var/www/hia/test/d3map/d3map.sh ]
+  then
+    ( cd /var/www/hia/test/d3map/ && ./d3map.sh )
+  fi
+#  sleep 10||break
+  if [ ! -x l.sh ] \
+  || [ ! -x walk.sh ]
+  then
+    break
+  fi
+done
+
