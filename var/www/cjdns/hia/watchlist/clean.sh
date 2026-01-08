9f7316e3805149cffa29c801c4cfc97a13eeba25
ircerr <ircerr@yahoo.ca>
2026-01-08 09:35:47 -0500

    added clean.sh

---
+++ var/www/cjdns/hia/watchlist/clean.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+USED=$(("`df . --output=pcent|tr -d ' \|%'|grep -v '^[A-Z]'`"))
+if [ "$USED" -lt 80 ]
+then
+  exit 0
+fi
+
+(
+
+USED=$(("`df . --output=pcent|tr -d ' \|%'|grep -v '^[A-Z]'`"))
+echo "--USED $USED % file space."
+echo "---Pruning old data"
+echo "c/olddata/: `du -ms c/olddata/|cut -f1`MB"
+sleep 1
+
+while :;
+do
+  USED=$(("`df . --output=pcent|tr -d ' \|%'|grep -v '^[A-Z]'`"))
+  if [ "$USED" -lt 80 ]
+  then
+    break
+  fi
+#  ls log/*xz | grep xz$ | sort -n | head -n10 | xargs rm -v
+  NF="`find c/olddata/ -type f | sort -n | head -n1`"
+  if [ "$NF" == "" ]
+  then
+    echo "----No more old data"
+    break
+  fi
+  S="`du -ms "\$NF\"|cut -f1`"
+  echo "-Cleaning \"$NF\" ${S}MB"
+  rm -rf "$NF"
+  sleep .01||break
+done
+USED=$(("`df . --output=pcent|tr -d ' \|%'|grep -v '^[A-Z]'`"))
+echo "--USED $USED % file space."
+) 2>&1 | tee -a clean.log
